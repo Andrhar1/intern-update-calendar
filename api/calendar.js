57fbf27e1b567189b08e92c4e819f75e7682e026
andri <andrihari0704@gmail.com>
2025-02-13 10:03:54 +0700

    fix vercel

--- api/calendar.js
+++ api/calendar.js
@@ -1,5 +1,5 @@
 import { google } from 'googleapis';
-import { NowRequest, NowResponse } from '@vercel/node';
+//import { NowRequest, NowResponse } from '@vercel/node';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
